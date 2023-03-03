126571a86349e7a238e3288358712fc76aa233c4
ArcherHuang <mmosconii@gmail.com>
2023-03-03 10:48:52 +0800

    Update

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    Event Hub Name: {{ eventHubName }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      eventHubName: process.env.VUE_APP_EVENTHUB_NAME,
+    };
+  },
+  components: {
+
+  },
+  methods: {
+
+  },
+};
+</script>
